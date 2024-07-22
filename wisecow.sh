46450d408292d923da613c6a0b2e1a949e91d14b
stark303test <86427704+stark303test@users.noreply.github.com>
2024-07-22 14:51:51 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -7,38 +7,40 @@ rm -f $RSPFILE
 mkfifo $RSPFILE
 
 get_api() {
-    read line
-    echo $line
+	read line
+	echo $line
 }
 
 handleRequest() {
-    get_api
-    mod=`fortune`
+    # 1) Process the request
+	get_api
+	mod=`fortune`
 
 cat <<EOF > $RSPFILE
-HTTP/1.1 200 OK
-Content-Type: text/html
+HTTP/1.1 200
+
 
 <pre>`cowsay $mod`</pre>
 EOF
 }
 
 prerequisites() {
-    command -v cowsay >/dev/null 2>&1 &&
-    command -v fortune >/dev/null 2>&1 || { 
-        echo "Install prerequisites."
-        exit 1
-    }
+	command -v cowsay >/dev/null 2>&1 &&
+	command -v fortune >/dev/null 2>&1 || 
+		{ 
+			echo "Install prerequisites."
+			exit 1
+		}
 }
 
 main() {
-    prerequisites
-    echo "Wisdom served on port=$SRVPORT..."
+	prerequisites
+	echo "Wisdom served on port=$SRVPORT..."
 
-    while [ 1 ]; do
-        cat $RSPFILE | nc -lkN $SRVPORT | handleRequest
-        sleep 0.01
-    done
+	while [ 1 ]; do
+		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+		sleep 0.01
+	done
 }
 
 main
